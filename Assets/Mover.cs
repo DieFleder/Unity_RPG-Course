6a39262a030eab53b42f1e1e513289d540f11855
Diefleder <the_laughing_bandit@hotmail.com>
2020-08-02 10:45:09 -0700

    S2L14 Impliment Click to move   - can now hold or click left mouse and player will move to the location of the click according to the navmesh constraints.

---
+++ Assets/Mover.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+    int rayLength = 100;
+
+    NavMeshAgent navMeshAgent;
+
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit)
+        {
+            navMeshAgent.destination = hit.point;
+        }
+    }
+}
